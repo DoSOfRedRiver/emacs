217597174ffb79ba76a62d6e516f437b00b2b5e5
dosofredriver <apyatin@hdwtech.com>
2016-02-10 14:56:01 +0600

    Add work.org file with themes for meeting. Change agenda-setting.el

--- .emacs.d/agenda-settings.el
+++ .emacs.d/agenda-settings.el
@@ -1,4 +1,23 @@
 (setq org-log-done t)
+(setq org-use-fast-todo-selection t)
 (setq org-agenda-files (list
 			"~/org/work.org"
 			"~/org/tasks.org"))
+
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
+
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "red" :weight bold)
+              ("NEXT" :foreground "blue" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold)
+              ("WAITING" :foreground "orange" :weight bold)
+              ("HOLD" :foreground "magenta" :weight bold)
+              ("CANCELLED" :foreground "forest green" :weight bold)
+              ("MEETING" :foreground "forest green" :weight bold)
+              ("PHONE" :foreground "forest green" :weight bold))))
+
+(add-hook 'org-after-todo-state-change-hook 
+	  (lambda () (cond ((eq org-state 'DONE) (print "hello")))))
+
